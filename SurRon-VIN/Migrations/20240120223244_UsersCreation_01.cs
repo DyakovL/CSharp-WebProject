19b8a4b49df564f26b416cb94fc91e9c235e3db5
DyakovL <114020620+DyakovL@users.noreply.github.com>
2024-01-21 00:55:24 +0200

    Beggining
    Added the Main Page also began the adding of a database for users

---
+++ SurRon-VIN/Migrations/20240120223244_UsersCreation_01.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SurRon_VIN.Migrations
+{
+    /// <inheritdoc />
+    public partial class UsersCreation_01 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(12)", maxLength: 12, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
--- SurRon-VIN/obj/Debug/net8.0/SurRon-VIN.AssemblyInfo.cs
+++ SurRon-VIN/obj/Debug/net8.0/SurRon-VIN.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SurRon-VIN")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73638573d32382fc6bb7a36ae945f8b2d1808cc5")]
 [assembly: System.Reflection.AssemblyProductAttribute("SurRon-VIN")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SurRon-VIN")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
