cb3089d522a036dc40dee487c507dc75d0e41c41
DyakovL <114020620+DyakovL@users.noreply.github.com>
2024-01-21 23:46:14 +0200

    Changes
    Small changes in the shared Layout and database

--- SurRon-VIN/Models/Users.cs
+++ SurRon-VIN/Models/Users.cs
@@ -13,6 +13,19 @@ namespace SurRon_VIN.Models
         public string? Email { get; set; }
 
         [Required]
+        public string? Username { get; set; }
+
+        [Required]
+        public string? FirstName { get; set; }
+
+        [Required]
+        public string? LastName { get; set; }
+
+        [Required]
+        [Phone]
+        public string? PhoneNumber { get; set; }
+
+        [Required]
         [MaxLength(12)]
         [MinLength(8)]
         [PasswordPropertyText]
